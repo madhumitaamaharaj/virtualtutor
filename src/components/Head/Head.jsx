2794a50b3d7e705bf2a76fbfc92c6ce55af72093
madhumitaamaharaj <madhumitaamaharaj7@gmail.com>
2023-11-06 22:29:12 +0530

    FEAT:HEADER,SIDEBAR added css

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -6,10 +6,14 @@ import { FaSearch } from 'react-icons/fa';
 import styles from './Head.module.css'; 
 
 const Head = () => {
+  const iconStyle = {
+    color: '#8b1f8b', 
+  };
+
   return (
     <div className={styles.headContainer}>
       <div className={styles.leftIcons}>
-        <GiHamburgerMenu className={styles.icon} /> 
+        <GiHamburgerMenu className={styles.icon} style={iconStyle} /> 
       </div>
 
       <div className={styles.centerIcons}>
@@ -20,14 +24,14 @@ const Head = () => {
           placeholder="Search"
           onChange={(e) => e.target.value}
         />
-        <button className={styles.searchButton}><FaSearch/></button>
+        <button className={styles.searchButton}><FaSearch style={iconStyle} /></button>
       </div>
     </div>
 
       <div className={styles.rightIcons}>
-        <BsBell className={styles.icon} /> 
+        <BsBell className={styles.icon} style={iconStyle} /> 
         <div className={styles.iconSpacing} /> 
-        <FaUserGraduate className={styles.icon} /> 
+        <FaUserGraduate className={styles.icon} style={iconStyle} /> 
       </div>
     </div>
   );
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { MdCastForEducation, MdSpaceDashboard } from 'react-icons/md';
+import { FaPowerOff } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
-    return (
-        <div className={styles.sidebar}>
+  const iconStyle = {
+    color: '#8b1f8b', 
+  };
+
+  return (
+    <div className={styles.sidebar}>
       <div className={styles.button}>
         <p>
-          <span className={styles.icon}><MdCastForEducation /></span>
-          Courses
+          <span className={styles.icon} style={iconStyle}><MdCastForEducation /></span>
+          <strong style={iconStyle}>Courses</strong> 
         </p>
       </div>
       <div className={styles.button}>
         <p>
-          <span className={styles.icon}><MdSpaceDashboard /></span>
-          Dashboard
+          <span className={styles.icon} style={iconStyle}><MdSpaceDashboard /></span>
+          <strong style={iconStyle}>Dashboard</strong> 
         </p>
       </div>
       <div className={styles.logoutButton}>
-        <button className="btn btn-primary">Logout</button>
+        <p>
+          <span className={styles.icon} style={iconStyle}><FaPowerOff /></span>
+          <strong style={iconStyle}>Logout</strong> 
+        </p>
       </div>
     </div>
   );
 };
-
-    
 
 export default Sidebar;
