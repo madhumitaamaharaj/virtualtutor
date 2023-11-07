457d77ab5cc71dfc9289bcdd152d70c43e43b88e
madhumitaamaharaj <madhumitaamaharaj7@gmail.com>
2023-11-07 20:47:01 +0530

    Fix:css of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MdCastForEducation, MdSpaceDashboard } from 'react-icons/md';
 import { FaPowerOff } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
@@ -11,12 +13,14 @@ const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.button}>
+      <Link to="/" className={styles.linkunderline}>
         <p>
           <span className={styles.icon} style={iconStyle}>
             <MdCastForEducation />
           </span>
           <strong style={iconStyle}>Courses</strong>
         </p>
+        </Link>
       </div>
       <div className={styles.button}>
         <p>
